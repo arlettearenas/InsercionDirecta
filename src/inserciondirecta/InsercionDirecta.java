a28f842562f7f8a91cce39aa6c53a0d60ac7d71e
arlette arenas <arlettearenas29@gmail.com>
2022-10-31 23:20:23 -0600

    Inserción Directa

---
+++ src/inserciondirecta/InsercionDirecta.java
@@ -0,0 +1,50 @@
+
+package inserciondirecta;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ * @author arlet
+ */
+public class InsercionDirecta {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int arreglo[], nElementos, pos, aux;
+        
+        nElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite el número de elementos "));
+        
+        arreglo = new int [nElementos];
+        
+        System.out.println("Digite el arreglo ");
+        for (int i = 0; i < nElementos; i++){
+            System.out.println((i+1)+ ". Digite un número");
+            arreglo[i] = entrada.nextInt();
+        }
+        //Ordenamiento por inserción
+        for(int i = 0; i < nElementos; i++){
+            pos = i;
+            aux = arreglo[i];
+            
+            while((pos>0) && (arreglo [pos-1] > aux)){
+                arreglo[pos] = arreglo [pos-1];
+                pos--;
+            }
+            arreglo[pos] = aux;
+        }
+         
+            System.out.print("\nOrden ascendente: ");
+            for (int i=0; i<nElementos;i++){
+                System.out.print(arreglo[i]+ ",");
+    }
+            System.out.println("");
+            
+            System.out.print("Orden desendente: ");
+            for (int i=(nElementos-1); i >= 0; i--){
+                System.out.print(arreglo[i] + ",");
+            }
+            System.out.println("");
+    }
+    
+}
